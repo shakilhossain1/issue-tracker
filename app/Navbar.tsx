3908317446ca6a2e881ac9ffaca03a52e5bc941b
Sk <shakil@meghops.com>
2024-12-23 22:16:27 +0600

    init

---
+++ app/Navbar.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { AiFillBug } from "react-icons/ai";
+
+const Navbar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <nav className="flex items-center space-x-6 border-b mb-5 px-5 h-14">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              href={link.href}
+              className="text-zinc-500 hover:text-zinc-800 transition"
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
