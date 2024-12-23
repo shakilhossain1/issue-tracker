535b0a14f70d83c0d70828a40ff9bbe4e762fb3c
Sk <shakil@meghops.com>
2024-12-23 22:25:24 +0600

    navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,11 +1,18 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
+import classNames from "classnames";
 
 const Navbar = () => {
+  const currentPath = usePathname();
+
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
+
   return (
     <nav className="flex items-center space-x-6 border-b mb-5 px-5 h-14">
       <Link href="/">
@@ -16,7 +23,11 @@ const Navbar = () => {
           <li key={link.href}>
             <Link
               href={link.href}
-              className="text-zinc-500 hover:text-zinc-800 transition"
+              className={classNames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
             >
               {link.label}
             </Link>
