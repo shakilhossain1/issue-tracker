960750eb5b07488062369adebdb9e7056e3087ba
Sk <shakil@meghops.com>
2024-12-24 01:05:50 +0600

    loading skeletons

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,7 +1,50 @@
+import { Button, Table } from "@radix-ui/themes";
 import React from "react";
+import IssueStatusBadge from "../components/IssueStatusBadge";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import Link from "next/link";
+import IssueActions from "./IssueActions";
 
 const LoadingIssuesPage = () => {
-  return <div>LoadingIssuesPage</div>;
+  const issues = [1, 2, 3, 4, 5];
+  return (
+    <div>
+      <IssueActions />
+
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue}>
+              <Table.Cell>
+                <Skeleton />
+                <div className="md:hidden">
+                  <Skeleton />
+                </div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                <Skeleton />
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                <Skeleton />
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </div>
+  );
 };
 
 export default LoadingIssuesPage;
