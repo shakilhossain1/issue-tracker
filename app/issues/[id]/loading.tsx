af457ed791d357387b2b74430217485e80fc66ac
Sk <shakil@meghops.com>
2024-12-24 01:50:57 +0600

    view issues

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Box, Card, Flex, Heading } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingIssueDetailPage = () => {
   return (
-    <div>Loading..</div>
-  )
-}
+    <Box className="max-w-xl">
+      <Heading>
+        <Skeleton  />
+      </Heading>
+      <Flex gap="3" className="items-center py-2">
+        <Skeleton width={"5rem"} />
+        <Skeleton width={"8rem"} />
+      </Flex>
+      <Card className="mt-5">
+        <Skeleton count={3} />
+      </Card>
+    </Box>
+  );
+};
 
-export default LoadingIssueDetailPage
+export default LoadingIssueDetailPage;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,7 @@
 import prisma from "@/prisma/client";
-import { Button, Table } from "@radix-ui/themes";
-import Link from "next/link";
-import React from "react";
+import { Table } from "@radix-ui/themes";
 import IssueStatusBadge from "../components/IssueStatusBadge";
-import delay from "delay";
+import Link from "../components/Link";
 import IssueActions from "./IssueActions";
 
 const IssuePage = async () => {
