280d49f6d5c21edcd1f7c7a63daa0d3b594e8f37
Sk <shakil@meghops.com>
2024-12-24 01:27:45 +0600

    bug details page

---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,33 @@
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import prisma from "@/prisma/client";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { notFound } from "next/navigation";
+import React from "react";
+import Markdown from "react-markdown";
+
+interface Props {
+  params: { id: string };
+}
+
+const IssueDetailsPage = async ({ params }: Props) => {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue) notFound();
+
+  return (
+    <div>
+      <Heading>{issue.title}</Heading>
+      <Flex gap="3" className="items-center py-2">
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card className="prose mt-5">
+        <Markdown>{issue.description}</Markdown>
+      </Card>
+    </div>
+  );
+};
+
+export default IssueDetailsPage;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -8,7 +8,6 @@ import IssueActions from "./IssueActions";
 
 const IssuePage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(2000);
 
   return (
     <div>
@@ -30,7 +29,7 @@ const IssuePage = async () => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                {issue.title}
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className="md:hidden">
                   <IssueStatusBadge status={issue.status} />
                 </div>
