2d1e90ea261e5571e3c56825e9fecfccd60021c5
Sk <shakil@meghops.com>
2024-12-26 09:34:47 +0600

    eslit issue

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 
 export async function GET(request: NextRequest) {
+  console.log(request);
   const users = await prisma.user.findMany({ orderBy: { name: "asc" } });
   return NextResponse.json(users);
 }
